1a98b5c18bd4ed72c08b175c218e17b8101696e3
kimi <2361641347@qq.com>
2023-11-28 15:53:09 +0800

    修改bug

--- app/lib/hosts.tsx
+++ app/lib/hosts.tsx
@@ -6,8 +6,8 @@ const env =
 		: 'development'
 
 export const LINK_HOST = {
-	production: `${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-	preview: `${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+	production: `link---${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+	preview: `link---${process.env.NEXT_PUBLIC_VERCEL_URL}`,
 	development: 'makereal-link.localhost:3000',
 }[env]
 
