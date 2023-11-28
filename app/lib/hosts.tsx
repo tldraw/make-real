95ca037cc8fa987c34f24315c3aac4458f9f45bd
kimi <2361641347@qq.com>
2023-11-28 16:50:13 +0800

    修改bug2

--- app/lib/hosts.tsx
+++ app/lib/hosts.tsx
@@ -7,8 +7,8 @@ const env =
 
 export const LINK_HOST = {
 	production: 'makereal.tldraw.link',
-	preview: `${process.env.NEXT_PUBLIC_VERCEL_URL}`,
-	development: 'makereal-link.localhost:3000',
+	preview: `link---${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+	development: 'www.kimi.asia',
 }[env]
 
 export const APP_HOST = {
