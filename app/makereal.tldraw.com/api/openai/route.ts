83e1f851a8685b67defce3d8cb136543b9424bf0
Steve Ruiz <steveruizok@gmail.com>
2024-06-25 19:36:53 +0100

    max durations

--- app/makereal.tldraw.com/api/openai/route.ts
+++ app/makereal.tldraw.com/api/openai/route.ts
@@ -1,6 +1,8 @@
 import { createOpenAI } from '@ai-sdk/openai'
 import { streamText } from 'ai'
 
+export const maxDuration = 120 // This function can run for a maximum of 5 seconds
+
 export async function POST(req: Request) {
 	const { apiKey, messages, model, systemPrompt } = await req.json()
 	const openai = createOpenAI({ apiKey })
