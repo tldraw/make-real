23b59c77e481db9e04066245d6501da11aff8138
Steve Ruiz <steveruizok@gmail.com>
2024-06-25 19:39:48 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	functions: {
+		'app/makereal.tldraw.com/api/**/*': {
+			maxDuration: 120, // All functions can run for a maximum of 120 seconds
+		},
+	},
+}
 
 module.exports = nextConfig
