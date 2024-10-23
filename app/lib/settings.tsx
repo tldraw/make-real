305278bed1f651e991a350b1db8319926395834d
Steve Ruiz <steveruizok@gmail.com>
2024-10-23 07:47:01 +0100

    Update settings.tsx

--- app/lib/settings.tsx
+++ app/lib/settings.tsx
@@ -13,6 +13,7 @@ export const PROVIDERS = [
 		id: 'anthropic',
 		name: 'Anthropic',
 		models: [
+			'claude-3-5-sonnet-20241022',
 			'claude-3-5-sonnet-20240620',
 			'claude-3-opus-20240229',
 			'claude-3-sonnet-20240229',
