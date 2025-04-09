a5b532b6e60c0c1f8b3b75d308e86ca029f275e8
Steve Ruiz <steveruizok@gmail.com>
2025-04-09 18:10:07 +0100

    Update settings.tsx

--- app/lib/settings.tsx
+++ app/lib/settings.tsx
@@ -28,7 +28,8 @@ export const PROVIDERS = [
 		id: 'google',
 		name: 'Google',
 		models: [
-			'gemini-2.5-pro-preview-03-25',
+			'gemini-2.5-pro-exp-03-25',
+			// 'gemini-2.5-pro-preview-03-25',
 			'gemini-2.0-flash',
 			'gemini-1.5-pro',
 			'gemini-1.5-flash',
